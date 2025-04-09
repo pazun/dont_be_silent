cfac5804465c2f7e0921b38131413799bea77b11
pazun <silantyevnikitaaa@gmail.com>
2025-04-09 08:51:41 +0500

    Aligned with collor theme. Added logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Layout, Menu, theme } from 'antd';
+import { Layout, Menu, theme, ConfigProvider } from 'antd';
 import { createBrowserRouter, RouterProvider, Link, Outlet, useNavigate } from 'react-router-dom';
+import Logo from '/Logo.svg';
+import styles from './styles/Header.module.css';
 import Counter from './components/Counter';
 import About from './components/About';
 import Donation from './components/Donation';
 const { Header, Content } = Layout;
-//add different names to menu items
+
 const items1 = [
   {
     key: '/',
@@ -20,42 +22,58 @@ const items1 = [
     label: 'About'
   }
 ];
+
+const customTheme = {
+  token: {
+    colorPrimary: '#DA2864',
+    colorBgContainer: '#F2F3EE',
+    colorBgLayout: '#F2F3EE',
+    colorText: '#DA2864',
+    colorTextSecondary: '#151515',
+    borderRadius: 6,
+    colorBgHeader: '#F2F3EE',
+  },
+};
+
 const App = () => {
   const navigate = useNavigate();
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
-
+  
   const handleMenuClick = (e) => {
     navigate(e.key);
   };
+
   return (
-    <Layout>
-      <Header style={{ display: 'flex', alignItems: 'center' }}>
-        <div className="demo-logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['1']}
-          items={items1}
-          style={{ flex: 1, minWidth: 0 }}
-          onClick={handleMenuClick}
-        />
-      </Header>
-      <Layout style={{ padding: '0 24px 24px' }}>
-        <Content
-          style={{
-            padding: 24,
-            margin: 0,
-            minHeight: 280,
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
-          }}
-        >
-          <Outlet />
-        </Content>
+    <ConfigProvider theme={customTheme}>
+      <Layout>
+        <Header style={{ display: 'flex', alignItems: 'center', background: customTheme.token.colorBgHeader }}>
+          <div className={styles['header-logo-container']}>
+            <img src={Logo} alt="Logo" className={styles['main-logo']} />
+            <img src="/Text.svg" alt="Text Logo" className={styles['text-logo']} />
+          </div>
+          <Menu
+            theme="light"
+            mode="horizontal"
+            defaultSelectedKeys={['1']}
+            items={items1}
+            style={{ flex: 1, minWidth: 0, background: 'transparent', justifyContent: 'flex-end' }}
+            onClick={handleMenuClick}
+          />
+        </Header>
+        <Layout style={{ padding: '0 24px 24px', background: customTheme.token.colorBgLayout }}>
+          <Content
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+              background: customTheme.token.colorBgContainer,
+              borderRadius: customTheme.token.borderRadius,
+            }}
+          >
+            <Outlet />
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </ConfigProvider>
   );
 };
 const HomePage = () => (
